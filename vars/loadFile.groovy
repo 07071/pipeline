cdca1e3f8dfdaf5ab1f853b4f0db73a8e62352f4
yanghoon <ssang276@hanmail.net>
2018-07-18 16:58:20 +0900

    Update loadFile.groovy

--- vars/loadFile.groovy
+++ vars/loadFile.groovy
@@ -6,7 +6,7 @@ def xxx(){
   echo "${retort.utils.Utils.class}"
   echo "${retort.utils.Utils.class.classLoader}"
   
-  def url = new File('file.jar').toURL();
+  def url = new File('classpath:snakeyaml-1.17.jar').toURL();
   echo "${url}"
   
   retort.utils.Utils.class.classLoader.addURL(url)
