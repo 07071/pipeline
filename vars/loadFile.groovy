7a1dcb814398f12ab70d239e025b0209dad1e364
yanghoon <ssang276@hanmail.net>
2018-07-18 16:53:44 +0900

    Update loadFile.groovy

--- vars/loadFile.groovy
+++ vars/loadFile.groovy
@@ -2,7 +2,8 @@
 def xxx(){
   echo "loadFile!!"
   
+  // https://gist.github.com/rodrigozrusso/67c7a18a30a86012ce7c
   echo "${retort.utils.Utils.class}"
   echo "${retort.utils.Utils.class.classLoader}"
-  echo "${retort.utils.Utils.class.classLoader.rootLoader}"
+  echo "${retort.utils.Utils.class.classLoader.addURL(new File('file.jar').toURL())}"
 }
